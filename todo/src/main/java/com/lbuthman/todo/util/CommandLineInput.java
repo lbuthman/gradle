5e11c86c855df288733145379e33578b042e6396
lbuthman <lbuthman@gmail.com>
2017-07-15 20:25:44 -0500

    add functionality to process command line commands

--- todo/src/main/java/com/lbuthman/todo/util/CommandLineInput.java
+++ todo/src/main/java/com/lbuthman/todo/util/CommandLineInput.java
@@ -1,7 +1,33 @@
 package com.lbuthman.todo.util;
 
-/**
- * Created by lbuthman on 7/15/17.
- */
-public class CommandLineInput {
+import java.util.Map;
+import java.util.HashMap;
+
+public enum CommandLineInput {
+    FIND_ALL('a'), FIND_BY_ID('f'), INSERT('i'), UPDATE('u'), DELETE('d'), EXIT('e');
+
+    private final static Map<Character, CommandLineInput> INPUTS;
+
+    static {
+        INPUTS = new HashMap<>();
+
+        for (CommandLineInput input: values()) {
+            INPUTS.put(input.getShortCmd(), input);
+        }
+    }
+
+    private final char shortCmd;
+
+    CommandLineInput(char shortCmd) {
+        this.shortCmd = shortCmd;
+    }
+
+    public char getShortCmd() {
+        return shortCmd;
+    }
+
+    public static CommandLineInput getCommandLineInputForInput(char input) {
+        return INPUTS.get(input);
+    }
+
 }
