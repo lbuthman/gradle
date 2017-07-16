679451f40e147b8c5f5a355e3b25e99493ed6b6c
lbuthman <lbuthman@gmail.com>
2017-07-15 20:26:24 -0500

    add main class to run the program

--- todo/src/main/java/com/lbuthman/todo/ToDoApp.java
+++ todo/src/main/java/com/lbuthman/todo/ToDoApp.java
@@ -1,7 +1,24 @@
 package com.lbuthman.todo;
 
-/**
- * Created by lbuthman on 7/15/17.
- */
+import com.lbuthman.todo.util.CommandLineInput;
+import com.lbuthman.todo.util.CommandLineInputHandler;
+
 public class ToDoApp {
+
+    public static final char DEFAULT_INPUT = '\u0000';
+
+    public static void main(String[] args) {
+
+        CommandLineInputHandler commandLineInputHandler = new CommandLineInputHandler();
+        char command = DEFAULT_INPUT;
+
+        while (CommandLineInput.EXIT.getShortCmd() != command) {
+            commandLineInputHandler.printOptions();
+            String input = commandLineInputHandler.readInput();
+            char[] inputChars = input.length() == 1 ? input.toCharArray() : new char[]{DEFAULT_INPUT};
+            command = inputChars[0];
+            CommandLineInput commandLineInput = CommandLineInput.getCommandLineInputForInput(command);
+            commandLineInputHandler.processInput(commandLineInput);
+        }
+    }
 }
